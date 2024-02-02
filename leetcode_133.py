7da9ea223bcbc653fa2509ecb27a823599ec47df
Shaunak-Gupta-dev <shaunak5424@gmail.com>
2024-02-02 13:04:19 +0530

    leetcode_133

---
+++ leetcode_133.py
@@ -0,0 +1,15 @@
+class Solution:
+    def cloneGraph(self, node: "Node") -> "Node":
+        oldToNew = {}
+
+        def dfs(node):
+            if node in oldToNew:
+                return oldToNew[node]
+
+            copy = Node(node.val)
+            oldToNew[node] = copy
+            for n in node.neighbors:
+                copy.neighbors.append(dfs(n))
+            return copy
+
+        return dfs(node) if node else None
